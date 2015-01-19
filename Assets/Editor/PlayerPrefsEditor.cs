07289e780113da8656d18c49c17656bd834fc784
sanukin39 <sanukin39@gmail.com>
2015-01-19 23:22:03 +0900

    Add PlayerPrefs Value Search Func

--- Assets/Editor/PlayerPrefsEditor.cs
+++ Assets/Editor/PlayerPrefsEditor.cs
@@ -1,11 +1,44 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class PlayerPrefsEditor {
+public class PlayerPrefsEditor : EditorWindow {
+
+    string stringKey = "";
+    string intKey = "";
+    string floatKey = "";
 
     [MenuItem("Tools/PlayerPrefs/DeleteAll")]
     static void DeleteAll(){
         PlayerPrefs.DeleteAll();
         Debug.Log("Delete All Data Of PlayerPrefs!!");
+    }
+
+    [MenuItem("Tools/PlayerPrefs/OpenEditor")]
+    static void OpenEditor(){
+        EditorWindow.GetWindow<PlayerPrefsEditor>("PlayrePrefsEditor");
+    }
+
+    void OnGUI(){
+        GUILayout.Label( "Input PlayerPrefs Key Here" );
+        GUILayout.Label( "String Value" );
+        stringKey = GUILayout.TextField( stringKey );
+        if(PlayerPrefs.HasKey(stringKey)){
+            string data = PlayerPrefs.GetString(stringKey);
+            GUILayout.Label(data);
+        }
+
+        GUILayout.Label("Int Value");
+        intKey = GUILayout.TextField(intKey);
+        if(PlayerPrefs.HasKey(intKey)){
+            string data = PlayerPrefs.GetInt(intKey).ToString();
+            GUILayout.Label(data);
+        }
+
+        GUILayout.Label("Float Value");
+        floatKey = GUILayout.TextField(floatKey);
+        if(PlayerPrefs.HasKey(floatKey)){
+            string data = PlayerPrefs.GetFloat(floatKey).ToString();
+            GUILayout.Label(data);
+        }
     }
 }
